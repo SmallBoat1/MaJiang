1ceffdaccac539d75dbdcf9c996a814d2e5ebff3
SmallBoat1 <2854688779@qq.com>
2017-04-06 22:13:17 +0800

    no message

---
+++ Client/Assets/Scripts/Frame/UI/Base/UIMapContainer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIObject{
+
+}
+
+public class UIMapContainer {
+
+	public UIMapContainer()
+	{
+
+	}
+
+	private Dictionary<string,UIObject> _map = new Dictionary<string,UIObject>();
+}
